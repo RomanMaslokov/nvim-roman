1d741718fd32ea4cc0bc03ee8ad7b783af0daf34
RomanMaslokov <168971033+RomanMaslokov@users.noreply.github.com>
2024-06-09 21:52:00 +0300

    Add files via upload

---
+++ lua/vim-options.lua
@@ -0,0 +1,8 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set smarttab")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set cursorline")
+vim.cmd("set guicursor=n-v-c-i:block")
+
+vim.wo.number = true
